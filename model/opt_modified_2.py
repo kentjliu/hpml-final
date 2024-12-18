52c6fedd27b641c3e70d49c7b443780fc15bd7c9
Kent Liu <kent.liu@rschooltoday.com>
2024-12-17 23:50:26 -0500

    f

--- model/opt_modified_2.py
+++ model/opt_modified_2.py
@@ -401,11 +401,11 @@ class OPTAttention_QJL(nn.Module):
             
             # Initialize QJL quantizer
             kv_quant = QJLKeyQuantizer(
-                self.qjl if idx >= self.initial_layers_count else self.qjl_initial_layers,
-                self.outlier_count_general if idx >= self.initial_layers_count else self.outlier_count_initial_layers,
+                self.qjl # if idx >= self.initial_layers_count else self.qjl_initial_layers,
+                self.outlier_count_general # if idx >= self.initial_layers_count else self.outlier_count_initial_layers,
                 self.buffer_size,
                 self.group_size,
-                self.key_quantization_bits if idx >= self.initial_layers_count else self.key_quantization_bits_initial_layers
+                self.key_quantization_bits # if idx >= self.initial_layers_count else self.key_quantization_bits_initial_layers
             )
             kv_quant.build_sketch(key_states)
 
